11b0d3da8d3330fb822048f9677723a9f2bb1c9e
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2025-01-15 09:06:27 +0700

    feat: add devices routing to dashboardRouter

--- src/lib/router/dashboardRouter.jsx
+++ src/lib/router/dashboardRouter.jsx
@@ -1,19 +1,24 @@
 import DashboardLayout from "../../layouts/DashboardLayout";
 import Dashboard from "../../pages/Dashboard";
 import Devices from "../../pages/Devices";
+import CreateDevice from "../../pages/CreateDevice";
+import DetailDevice from "../../pages/DetailDevice";
 
 const dashboardRouter = {
-  path: "/dashboard",
+  path: "/",
   element: <DashboardLayout />,
   children: [
     {
       index: true,
+      path: "/dashboard",
       element: <Dashboard />,
     },
     {
       path: "devices",
       element: <Devices />,
     },
+    { path: "devices/add", element: <CreateDevice /> },
+    { path: "devices/:id", element: <DetailDevice /> },
   ],
 };
 
